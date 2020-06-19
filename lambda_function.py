49fe42e4a3a0fc9cfb05e6777acc670c6ca71ba4
TAKEHARA_Ryuji <kozayupapa@gmail.com>
2020-06-20 08:27:12 +0900

    add test execution setting

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,15 @@ from download_pubdata import download
 from convert_pubdata import convert
 from uploadS3 import uploadS3
  
+
 def lambda_handler(event, context):
     print(event, context)
     download(False)
     data = convert(False)
-    uploadS3(data)
+
+    if(event['detail-type'] == 'test'):
+        # 本番反映前のテスト用、AWS Web lambda Consol
+        # テストイベント設定で"event-type":"test"を設定
+        uploadS3(data, "DataPubTest.json")
+    else:
+        uploadS3(data, "DataPub.json")
--- uploadS3.py
+++ uploadS3.py
@@ -2,9 +2,8 @@ import boto3
 import json
 s3 = boto3.resource('s3')
 
-def uploadS3(data):
+def uploadS3(data,key='DataPub.json'):
     bucket = 'covid19chiba'
-    key = 'DataPub.json'
     obj = s3.Object(bucket, key)
     obj.put( Body=json.dumps(data),ACL='public-read')
     return
